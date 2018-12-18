ba9feabf8530447dcff67ed2a4ef6c24796ab940
tj <oliver8709@gmail.com>
2018-12-17 21:23:50 -0500

    Added layout for camera fragment.

--- feature/src/main/java/ca/tjweb/watcher/feature/CameraFragment.java
+++ feature/src/main/java/ca/tjweb/watcher/feature/CameraFragment.java
@@ -4,6 +4,9 @@ import android.support.v4.app.ActivityCompat;
 
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.os.Bundle;
 
 
 public class CameraFragment extends Fragment
@@ -11,6 +14,11 @@ public class CameraFragment extends Fragment
 
     public static CameraFragment newInstance() {
         return new CameraFragment();
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_camera, container, false);
     }
 
     @Override
